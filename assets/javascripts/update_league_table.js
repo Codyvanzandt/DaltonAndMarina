2deaf93a4bf6fbc49d9f790ee116a55ddabdc170
Cody VanZandt <cody.a.vanzandt@gmail.com>
2020-05-14 21:12:46 -0500

    Update update_league_table.js

--- assets/javascripts/update_league_table.js
+++ assets/javascripts/update_league_table.js
@@ -2,12 +2,20 @@ function updateWithLeagueData(leagueData, tableID){
     var tableRef = document.getElementById(tableID).getElementsByTagName('tbody')[0];
 
     for (var i = 0, l = leagueData.length; i < l; i++) {
-        var newRow = tableRef.insertRow(-1);
         var gameData = leagueData[i];
+        var newRow = tableRef.insertRow(-1);
+        if( gameData["matchResult"] ){
+            newRow.setAttribute("style", "background-color:rgba(0, 255, 0, 0.3);")
+        } else {
+            newRow.setAttribute("style", "background-color:rgba(255, 0, 0, 0.3);")
+        }
+        
         for (const [index, [key, value]] of Object.entries(Object.entries(gameData))) {
-            var newCell  = newRow.insertCell(index);
-            var newText  = document.createTextNode(value);
-            newCell.appendChild(newText);
+            if( key != "matchResult" ){
+                var newCell  = newRow.insertCell(index);
+                var newText  = document.createTextNode(value);
+                newCell.appendChild(newText);
+            }  
         }
     }
 }
